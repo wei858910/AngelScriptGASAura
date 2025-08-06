8fc0c1f1594fd709902b4c71e19f6674f1cb984f
Weiwei <93496699+wei858910@users.noreply.github.com>
2025-08-06 14:54:29 +0800

    暴露 SetReplicationMode 接口给脚本调用

---
+++ Source/Aura/Mixin/AuraAbilitySystemComponent.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "UObject/Object.h"
+
+#include "AuraAbilitySystemComponent.generated.h"
+
+UCLASS(Meta = (ScriptMixin = "UAbilitySystemComponent"))
+class AURA_API UAngelScriptProgressBar : public UObject
+{
+    GENERATED_BODY()
+
+    public:
+    UFUNCTION(ScriptCallable)
+    static void SetReplicationMode(UAbilitySystemComponent* ASC, const EGameplayEffectReplicationMode NewReplicationMode)
+    {
+        ASC->SetReplicationMode(NewReplicationMode);
+    }
+};
